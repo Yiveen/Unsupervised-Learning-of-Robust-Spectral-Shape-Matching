cbbd6f3b1bd4538258d8791b277f8c4a74a14520
Yiveen <384088746@qq.com>
2024-04-20 23:56:22 +0200

    first_change

--- back-to-3d-few-shot-keypoints/back-to-3d-few-shot-keypoints/feature_backprojection/__init__.py
+++
@@ -1,9 +0,0 @@
-from .backprojection import compute_kp_dists_features, features_from_views
-from .model_wrappers import CLIPWrapper, DINOWrapper, EffNetWrapper, SAMWrapper
-
-models = {
-    "clip": CLIPWrapper,
-    "dino": DINOWrapper,
-    "effnet": EffNetWrapper,
-    "sam": SAMWrapper
-}
